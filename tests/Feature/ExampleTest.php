2c7846bd62a3bb61bb3d1ff42cc65e61212f9383
JNietoR <jorge.nieto35@gmail.com>
2023-10-30 20:30:27 +0100

    pruebas simples

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -10,10 +10,16 @@ class ExampleTest extends TestCase
     /**
      * A basic test example.
      */
-    public function test_the_application_returns_a_successful_response(): void
+    public function test_home()
     {
         $response = $this->get('/');
 
         $response->assertStatus(200);
+    }
+    public function test_products()
+    {
+        $response = $this->get('/products');
+
+        $response->assertStatus(404);
     }
 }
--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -9,8 +9,16 @@ class ExampleTest extends TestCase
     /**
      * A basic test example.
      */
-    public function test_that_true_is_true(): void
+
+    public function test_example_unit()
     {
         $this->assertTrue(true);
+        
+    }
+    public function test_math()
+    {
+        $sum = 5 + 5;
+
+        $this->assertEquals(10, $sum);
     }
 }
