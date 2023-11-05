006e6fb5897b78e5b9791bd83426b153ea12958d
JNietoR <jorge.nieto35@gmail.com>
2023-11-05 22:34:56 +0100

    vista create con tailwindcss

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,10 +5,16 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Laravel</title>
-
+        <script src="https://cdn.tailwindcss.com"></script>
         
     </head>
     <body>
-        <a href="{{ route('products.index') }}">Productos</a>
+        <div class="max-w-md mx-auto px-4">
+            <h1 class="text-2xl text-center my-8">
+                Administración de productos
+            </h1>
+
+            @yield('content')
+        </div>
     </body>
  </html>
