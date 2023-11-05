f465d660b69e13167772a88e13cdfa13f8edf7f9
JNietoR <jorge.nieto35@gmail.com>
2023-11-05 22:46:40 +0100

    tailwind css en vistas index, create y edit

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -3,7 +3,15 @@
 @section('content')
 <form action="{{ route('products.store') }}" method="POST">
     @csrf
-    <input type="text" name="name" class="w-full py-2 border border-gray-300 rounded mb-4">
-    <input type="submit" value="Guardar" class="w-full bg-blue-600 rounded text-white px-4 py-2">
+    <input
+        type="text"
+        name="name"
+        class="w-full py-2 border border-gray-300 rounded mb-4"
+    >
+    <input
+        type="submit"
+        value="Guardar"
+        class="w-full bg-blue-600 rounded text-white px-4 py-2"
+    >
 </form>
 @endsection
