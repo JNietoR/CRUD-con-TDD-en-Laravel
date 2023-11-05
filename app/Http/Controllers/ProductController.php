9aead1d2b5e6763bb2be301f07524a4f1e74dcad
JNietoR <jorge.nieto35@gmail.com>
2023-11-05 22:08:55 +0100

    test destroy y función en el controlador

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -51,5 +51,12 @@ class ProductController extends Controller
 
         return redirect()->route('products.index');
     }
+
+    public function destroy(Product $product)
+    {
+        $product->delete();
+        
+        return redirect()->route('products.index');
+    }
     
 }
--- tests/Feature/Http/Controllers/ProductController/DestroyTest.php
+++ tests/Feature/Http/Controllers/ProductController/DestroyTest.php
@@ -6,15 +6,22 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
+use App\Models\Product;
+
 class DestroyTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    public function test_destroy()
+    {
+        $product = Product::factory()->create();
+
+        $this
+            //elimina el producto 
+            ->delete(route('products.destroy', $product))
+            //redirige al index despues de borrar el producto
+            ->assertRedirect(route('products.index'));
+        //comprueba que el producto se ha eliminado de la base de datos
+        $this->assertDatabaseMissing('products', $product->toArray());
     }
 }
